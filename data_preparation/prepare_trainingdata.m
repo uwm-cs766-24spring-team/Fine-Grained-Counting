b6e273498b701aab970a40ea00da8227ffd8a568
Mondo <mondo.jiang@wisc.edu>
2024-04-30 16:45:58 -0500

    feat: generate density map for towards

--- data_preparation/prepare_trainingdata.m
+++ data_preparation/prepare_trainingdata.m
@@ -4,33 +4,50 @@
 
 
 clc; clear all;
-dataset = 'B';
-dataset_name = ['shanghaitech_part_' dataset ];
-path = ['../data/original/shanghaitech/part_' dataset '_final/test_data/images/'];
-gt_path = ['../data/original/shanghaitech/part_' dataset '_final/test_data/ground_truth/'];
-gt_path_csv = ['../data/original/shanghaitech/part_' dataset '_final/test_data/ground_truth_csv/'];
+cd(fileparts(mfilename('fullpath')));
+cd ../../../
 
-mkdir(gt_path_csv )
-if (dataset == 'A')
-    num_images = 182;
-else
-    num_images = 316;
+dataset_name = 'Towards_vs_Away';
+current_path = pwd;
+
+path_prefix = ['dataset/Fine-Grained-Counting-Dataset/' dataset_name '/'];
+path_prefix_abs = fullfile(current_path, path_prefix);
+path = fullfile(path_prefix_abs, 'images/');
+gt_path_prefix = fullfile(path_prefix_abs, 'ground_truth/');
+dm_path_prefix = fullfile(path_prefix_abs, 'density_maps/');
+
+fprintf('Path: %s\n', gt_path_prefix);
+
+gt_files = dir([gt_path_prefix '*.mat']);
+num_files = length(gt_files);
+img_suffix = '.jpg';
+
+if (dataset_name == 'Towards_vs_Away')
+    img_suffix = '.png';
 end
 
-for i = 1:num_images    
+fprintf('Number of files: %d\n', num_files);
+
+mkdir(dm_path_prefix)
+
+for i = 1:num_files
     if (mod(i,10)==0)
-        fprintf(1,'Processing %3d/%d files\n', i, num_images);
+        fprintf(1,'Processing %3d/%d files\n', i, num_files);
     end
-    load(strcat(gt_path, 'GT_IMG_',num2str(i),'.mat')) ;
-    input_img_name = strcat(path,'IMG_',num2str(i),'.jpg');
+    gt_file_name = gt_files(i).name;
+    filename = gt_file_name(1:end-4);
+    
+    load(strcat(gt_path_prefix, gt_file_name)) ;
+    
+    input_img_name = strcat(path, filename,img_suffix);
     im = imread(input_img_name);
     [h, w, c] = size(im);
     if (c == 3)
         im = rgb2gray(im);
-    end     
-    annPoints =  image_info{1}.location;   
+    end
+    annPoints = towards;
     [h, w, c] = size(im);
-    im_density = get_density_map_gaussian(im,annPoints, 4,15˛);    
-    csvwrite([gt_path_csv ,'IMG_',num2str(i) '.csv'], im_density);       
+    im_density = get_density_map_gaussian(im,annPoints, 4,15);
+    csvwrite([gt_path_csv ,filename '.csv'], im_density);
 end
 
